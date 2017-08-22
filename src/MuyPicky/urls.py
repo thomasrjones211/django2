135db5052836236e3f94063828d7f029e7702819
Thomas Jones <thomasrjones211@gmail.com>
2017-08-22 00:34:12 -0600

    up to timestamp

--- src/MuyPicky/urls.py
+++ src/MuyPicky/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import home, home2, home3
+from django.views.generic import TemplateView
+from restaurants.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-    url(r'^home2/$', home2),
-    url(r'^home3/$', home3),
+    url(r'^$', HomeView.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,6 +3,29 @@ from __future__ import unicode_literals
 import random
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+
+        num = None
+        some_list = [
+            random.randint(0, 10000000), random.randint(0, 10000000), random.randint(0, 10000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 10000000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
+
+
 
 # rendering HTML string
 # def home(request):
@@ -20,28 +43,59 @@ from django.shortcuts import render
 #    </html>
 #    """
 #    return HttpResponse(html_)
-
-#Creating templates
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 10000000), random.randint(0, 10000000), random.randint(0, 10000000)
-    ]
-    condition_bool_item = True
-    if condition_bool_item:
-        num = random.randint(0, 10000000)
-    context = {
-        "num": num,
-        "some_list": some_list
-    }
-    return render(request, "base.html", context)
-
-def home2(request):
-    context = {
-    }
-    return render(request, "home2.html", context)
-
-def home3(request):
-    context = {
-    }
-    return render(request, "home3.html", context)
+#
+# Creating templates
+# def home(request):
+#     num = None
+#     some_list = [
+#         random.randint(0, 10000000), random.randint(0, 10000000), random.randint(0, 10000000)
+#     ]
+#     condition_bool_item = True
+#     if condition_bool_item:
+#         num = random.randint(0, 10000000)
+#     context = {
+#         "num": num,
+#         "some_list": some_list
+#     }
+#     return render(request, "home.html", context)
+#
+# def about(request):
+#     context = {
+#     }
+#     return render(request, "about.html", context)
+#
+# def contact(request):
+#     context = {
+#     }
+#     return render(request, "contact.html", context)
+#
+# class ContactView(View):
+#     def get(self, request, *args, **kwargs):
+#         context = {}
+#         return render(request, "contact.html", context)
+#
+# class HomeView(TemplateView):
+#     template_name = 'home.html'
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(HomeView, self).get_context_data(*args, **kwargs)
+#
+#         num = None
+#         some_list = [
+#             random.randint(0, 10000000), random.randint(0, 10000000), random.randint(0, 10000000)
+#         ]
+#         condition_bool_item = True
+#         if condition_bool_item:
+#             num = random.randint(0, 10000000)
+#         context = {
+#             "num": num,
+#             "some_list": some_list
+#         }
+#
+#         return context
+#
+# class AboutView(TemplateView):
+#     template_name = 'about.html'
+#
+# class ContactView(TemplateView):
+#     template_name = 'contact.html'
